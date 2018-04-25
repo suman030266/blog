f50ae66dbf6ce84fae13d6c893c222f3e4c5d8ae
suman <suman@gomeplus.com>
2018-04-25 12:30:16 +0800

    增加静态文件 修改server

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const url = require('url');
 
 let server = http.createServer((req, res)=>{
 	let {pathname, query} = url.parse(req.url, true);
-	if(pathname == '/' || pathname == '/index'){
-		 res.end('hello 9001 blog');
+	if(pathname == '/api/getData'){
+		let obj = {
+			data: 'datas',
+			code: 0
+		};
+		res.end(JSON.stringify(obj));
 	}else{
 		 res.end('404 not found');
 	}
 }).listen('9001',function (){
      console.log('9001 start work');
 });
-
